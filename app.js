749cd2e56b96daade94cad43eda68887ad770aef
iperich <iperich@debian.home>
2015-10-12 10:55:02 -0300

    editar nombre nodos

--- app.js
+++ app.js
@@ -85,6 +85,7 @@ mainApp.controller("mainController", function($scope,$sce) {
       parent_node.push({name:$scope.node_name,value:'test'});
       $scope.node_name="";
       $scope.show_add_root=false;
+      $scope.show_edit=false;
       $scope.treeDisplay($scope.tree.nodes,0)
    };
     
@@ -96,7 +97,7 @@ mainApp.controller("mainController", function($scope,$sce) {
      */
    $scope.del_node=function(node_string){
       
-      var indexes=node_string.split(".");
+      var indexes=node_string.split("_");
       
       var parent_string="$scope.tree.nodes";
       
@@ -115,8 +116,44 @@ mainApp.controller("mainController", function($scope,$sce) {
       $scope.treeDisplay($scope.tree.nodes,0)
     }
     
+       /*
+     *function edit_node:  delete nodes
+     *inputs:   parent_node: parent node
+     *          name: name of node to delete
+     *          
+     */
+   $scope.edit_node=function(node_string){
+      console.log("here");
+      var indexes=node_string.split("_");
+      
+      var node_array_string="$scope.tree.nodes";
+      
+      for (i = 0; i < indexes.length-1; i++) { 
+         index=indexes[i];
+         node_array_string=node_array_string+'['+index+'].children';
+      }
+      
+      var node=eval(node_array_string);
+      
+      var new_name=eval('$scope.edit_'+node_string);
+      console.log('$scope.edit_'+node_string);
+      node[indexes[indexes.length-1]].name=new_name;
+      eval('$scope.show_edit_'+node_string+'=false');
+      console.log()
+      
+      $scope.treeDisplay($scope.tree.nodes,0)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     /*
-    //funcion recursiva para recorrer el arbol completo
+    *funcion para dibujar el arbol
+    *
     */
     
     
@@ -134,11 +171,13 @@ mainApp.controller("mainController", function($scope,$sce) {
             for(var j = 0; j < depth; j++){
                 depth_view=depth_view+"--";
             }
-            $scope.tree_view = $scope.tree_view + (depth_view+current.name+'<a href="#" ng-click="del_node(\''+parent+i+'\');" ng-confirm-click="Sure?">Delete</a> <br>');
+            
+            $scope.tree_view = $scope.tree_view + (depth_view+'<span ng-show="!show_edit_'+parent+i+'">'+current.name+'</span><span ng-show="show_edit_'+parent+i+'"><input type="text" ng-model="edit_'+parent+i+'" name="edit_'+parent+i+'" value="'+current.name+'"><input type="button" ng-click="edit_node(\''+parent+i+'\')" value="Save"> </span><a href="#" ng-show="!show_edit_'+parent+i+'" ng-click="show_edit_'+parent+i+'=true">[Edit]</a><a href="#" ng-click="del_node(\''+parent+i+'\');" ng-confirm-click="Sure?">[Delete]</a> <br>');
             parentid = current.id == null ? '0' : current.id;
+            eval('$scope.edit_'+parent+i+'="'+current.name+'"');
             current.index = i;
             if (current.children && current.children.length > 0) {
-                $scope.treeDisplay(current.children,depth+1,parent+i+'.');
+                $scope.treeDisplay(current.children,depth+1,parent+i+'_');
             };
         };
       console.log($scope.tree_view);
